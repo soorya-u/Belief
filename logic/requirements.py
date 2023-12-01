f39609efd47ebb57a9a85f5b4a3c2cc871174f7b
Soorya U <sooryau7@gmail.com>
2023-12-01 21:17:24 +0530

    Removed Extra File

--- logic/requirements.py
+++
@@ -1,9 +0,0 @@
-import nltk
-
-if __name__ == "__main__":
-    
-    nltk.download("vader_lexicon")
-    nltk.download('wordnet')
-    nltk.download('stopwords')
-    nltk.download('punkt')
-    nltk.download('omw-1.4')
