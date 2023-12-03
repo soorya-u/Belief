765b80549c0cb4731ac183ed45d8f63ba34a7966
Soorya U <sooryau7@gmail.com>
2023-12-03 14:04:14 +0530

    Added Logic to Stats Route

--- app.py
+++ app.py
@@ -33,7 +33,27 @@ def about():
 
 @app.route("/stats", methods=['GET', 'POST'])
 def stats():
-    return render_template('stats.html')
+
+    tweet = ''
+    accuracy_score = 0
+    confusion_matrix_path = ''
+    overall_prediction = ['']
+    pos = ''
+    neu = ''
+    neg = ''
+
+    if request.method == 'POST' and request.form['tweet'] != '':
+        tweet = request.form['tweet']
+        model_name = request.form['model-name']
+        pre = PreProcessor(tweet)
+        pr = pre.preProcess()
+        algo = MLAlgorithms(model_name=model_name)
+        accuracy_score = algo.accuracy_score
+        confusion_matrix_path = algo.confusion_matrix
+        overall_prediction = algo.getOverallPrediction(pr)
+        pos, neu, neg = algo.getPercentagePrediction(pr)
+
+    return render_template('stats.html', output=overall_prediction[0], value=tweet, positive=pos, neutral=neu, negative=neg, accuracy_score = accuracy_score, img_path=confusion_matrix_path)
 
 
 if __name__ == "__main__":
