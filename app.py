ad4f5b45ccf8611a5c05f5d16fde571d778fd1c6
Soorya U <sooryau7@gmail.com>
2023-12-03 12:01:04 +0530

    Fixed Minor Bug

--- app.py
+++ app.py
@@ -8,24 +8,28 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    tweet=''
-    overall_prediction=['']
-    pos= ''
-    neu= ''
-    neg= ''
-    if request.method == 'POST' and request.form['tweet']!='':
+
+    tweet = ''
+    overall_prediction = ['']
+    pos = ''
+    neu = ''
+    neg = ''
+
+    if request.method == 'POST' and request.form['tweet'] != '':
         tweet = request.form['tweet']
         pre = PreProcessor(tweet)
         pr = pre.preProcess()
-        algo = MLAlgorithms(path=r'./logic')
+        algo = MLAlgorithms()
         overall_prediction = algo.getOverallPrediction(pr)
         pos, neu, neg = algo.getPercentagePrediction(pr)
 
     return render_template('index.html', output=overall_prediction[0], value=tweet, positive=pos, neutral=neu, negative=neg)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html')
+
 
 @app.route("/stats", methods=['GET', 'POST'])
 def stats():
