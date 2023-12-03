393ca411f6483a4fa724984c6be696fc4d4e43eb
Soorya U <sooryau7@gmail.com>
2023-12-03 17:37:31 +0530

    Configure Waitress

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ def stats():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=7000)
-    # serve(app, host='0.0.0.0', port=7000)
+    serve(app, host='0.0.0.0', port=7000) # For Production
+    # app.run(debug=True, port=7000) # For Development
