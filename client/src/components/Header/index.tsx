1d2c3ccb7cd062a6048fd277fda66fc7a22a2f8a
Soorya U <sooryau7@gmail.com>
2023-12-21 20:15:45 +0530

    Completed Header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,9 +1,36 @@
-import './styles.css'
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/img/logo_belief.png";
+import "./styles.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <header>
+        <nav>
+          <a href="https://github.com/soorya-u/Sentiment-Analysis">
+            <img className="logo" src={logo} alt="logo" />
+          </a>
+          <ul>
+            <li>
+              <NavLink to="/" end>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" end>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/stats" end>
+                Stats
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
