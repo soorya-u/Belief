c2b4d763c865e67138352a855c26149dd7dcdf73
Soorya U <sooryau7@gmail.com>
2023-12-21 20:15:14 +0530

    Added Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import About from "./components/About";
+import Home from "./components/Home";
+import Stats from "./components/Stats";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/stats" element={<Stats />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
