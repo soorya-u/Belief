eb5217f6475a766babd51aed053cc02e385cd1e3
Soorya U <sooryau7@gmail.com>
2023-12-22 20:51:22 +0530

    Modified Backend

--- server/app.py
+++ server/app.py
@@ -1,35 +1,34 @@
 from logic.preprocess import *
 from logic.algorithms import *
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 from waitress import serve
 
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['POST'])
 def home():
 
-    tweet = ''
-    overall_prediction = ['']
-    pos = ''
-    neu = ''
-    neg = ''
+    if request.json['tweet'] == '':
+        return jsonify({
+            'error': 'Empty String'
+        })
 
-    if request.method == 'POST' and request.form['tweet'] != '':
-        tweet = request.form['tweet']
-        pre = PreProcessor(tweet)
-        pr = pre.preProcess()
-        algo = MLAlgorithms()
-        overall_prediction = algo.getOverallPrediction(pr)
-        pos, neu, neg = algo.getPercentagePrediction(pr)
+    tweet = request.json['tweet']
+    pre = PreProcessor(tweet)
+    pr = pre.preProcess()
+    algo = MLAlgorithms()
+    overall_prediction = algo.getOverallPrediction(pr)
+    pos, neu, neg = algo.getPercentagePrediction(pr)
 
-    return render_template('index.html', isHome=True, output=overall_prediction[0], value=tweet, positive=pos, neutral=neu, negative=neg)
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html', isAbout=True)
+    return jsonify({
+        'output': overall_prediction[0],
+        'value': tweet,
+        'positive': pos,
+        'neutral': neu,
+        'negative': neg
+    })
 
 
 @app.route("/stats", methods=['GET', 'POST'])
@@ -44,20 +43,33 @@ def stats():
     neu = ''
     neg = ''
 
-    if request.method == 'POST' and request.form['tweet'] != '':
-        tweet = request.form['tweet']
-        model_name = request.form['model-name']
-        pre = PreProcessor(tweet)
-        pr = pre.preProcess()
-        algo = MLAlgorithms(model_name=model_name)
-        accuracy_score = algo.accuracy_score
-        confusion_matrix_path = algo.confusion_matrix
-        overall_prediction = algo.getOverallPrediction(pr)
-        pos, neu, neg = algo.getPercentagePrediction(pr)
+    if request.json['tweet'] == '':
+        return jsonify({
+            'error': 'Empty String'
+        })
 
-    return render_template('stats.html', isStats=True, output=overall_prediction[0], value=tweet, positive=pos, neutral=neu, negative=neg, accuracy_score=accuracy_score, img_path=confusion_matrix_path, model_name=model_name)
+    tweet = request.json['tweet']
+    model_name = request.json['model_name']
+    pre = PreProcessor(tweet)
+    pr = pre.preProcess()
+    algo = MLAlgorithms(model_name=model_name)
+    accuracy_score = algo.accuracy_score
+    confusion_matrix_path = algo.confusion_matrix
+    overall_prediction = algo.getOverallPrediction(pr)
+    pos, neu, neg = algo.getPercentagePrediction(pr)
+
+    return jsonify({
+        'output': overall_prediction[0],
+        'value': tweet,
+        'positive': pos,
+        'neutral': neu,
+        'negative': neg,
+        'accuracy_score': accuracy_score,
+        'img_path': confusion_matrix_path,
+        'model_name': model_name
+    })
 
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=7000) # For Production
-    # app.run(debug=True, port=7000) # For Development
+    # serve(app, host='0.0.0.0', port=7000) # For Production
+    app.run(debug=True, port=7000)  # For Development
