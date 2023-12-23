5e0e10654bb410ff501ab468a554e4377ac75da6
Soorya U <sooryau7@gmail.com>
2023-12-23 15:17:29 +0530

    Added PyMongo

--- server/app.py
+++ server/app.py
@@ -1,13 +1,24 @@
-from logic.preprocess import *
-from logic.algorithms import *
+import os
+from dotenv import load_dotenv
+
+import pymongo
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+
 from waitress import serve
 
+from logic.preprocess import *
+from logic.algorithms import *
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 app = Flask(__name__)
-
 CORS(app)
+
+client = pymongo.MongoClient(DATABASE_URL)
+dataBase = client['Belief']
 
 
 @app.route("/", methods=['POST'])
@@ -18,19 +29,29 @@ def home():
             'error': 'Empty String'
         })
 
+    collection = dataBase['main-algorithms']
+
     tweet = request.json['tweet']
+
     pre = PreProcessor(tweet)
     pr = pre.preProcess()
     algo = MLAlgorithms()
+
     overall_prediction = algo.getOverallPrediction(pr)
     pos, neu, neg = algo.getPercentagePrediction(pr)
 
-    return jsonify({
+    payload = {
         'output': overall_prediction[0],
         'positive': pos,
         'neutral': neu,
         'negative': neg
-    })
+    }
+
+    response = jsonify(payload)
+
+    payload['tweet'] = tweet
+    collection.insert_one(payload)
+    return response
 
 
 @app.route("/stats", methods=['GET', 'POST'])
@@ -41,17 +62,21 @@ def stats():
             'error': 'Empty String'
         })
 
+    collection = dataBase['other-algorithms']
+
     tweet = request.json['tweet']
     model_name = request.json['model_name']
+
     pre = PreProcessor(tweet)
     pr = pre.preProcess()
     algo = MLAlgorithms(model_name=model_name)
+
     accuracy_score = algo.accuracy_score
     confusion_matrix_path = algo.confusion_matrix[1:]
     overall_prediction = algo.getOverallPrediction(pr)
     pos, neu, neg = algo.getPercentagePrediction(pr)
 
-    return jsonify({
+    payload = {
         'output': overall_prediction[0],
         'positive': pos,
         'neutral': neu,
@@ -59,7 +84,14 @@ def stats():
         'accuracy_score': accuracy_score,
         'img_path': confusion_matrix_path,
         'model_name': model_name
-    })
+    }
+
+    response = jsonify(payload)
+
+    payload['tweet'] = tweet
+    collection.insert_one(payload)
+
+    return response
 
 
 if __name__ == '__main__':
