655f24c30c46955541d674f2f8305a1cc3450a3c
Soorya U <sooryau7@gmail.com>
2023-12-25 13:51:34 +0530

    Added Database Module

--- server/app.py
+++ server/app.py
@@ -1,45 +1,59 @@
+# Environment Modules
 import os
 from dotenv import load_dotenv
 
-import pymongo
+# Database Modules
+from pymongo import MongoClient
 
+# Backend Modules
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+# WSGI Modules
 from waitress import serve
 
+# Defined Modules
 from logic.preprocess import *
 from logic.algorithms import *
+from database.mongo import *
 
+# Environment Files
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
+PORT = os.getenv("PORT")
 
+# Backend Framework and CORS
 app = Flask(__name__)
 CORS(app)
 
-client = pymongo.MongoClient(DATABASE_URL)
-dataBase = client['Belief']
+# Database Client and Database
+client = MongoClient(DATABASE_URL)
+database = Database(client['Belief'])
 
 
-@app.route("/api/v1/", methods=['POST'])
+# Main Route
+@app.route("/api/v1/main", methods=['POST'])
 def home():
 
+    # Handling Request Body Requirements
     if request.json['tweet'] == '':
         return jsonify({
             'error': 'Empty String'
         })
 
-    collection = dataBase['main-algorithm']
-
+    # Getting Body
     tweet = request.json['tweet']
 
+    # Object Init
     pre = PreProcessor(tweet)
     pr = pre.preProcess()
     algo = MLAlgorithms()
 
+    # Applying Algorithms
     overall_prediction = algo.getOverallPrediction(pr)
     pos, neu, neg = algo.getPercentagePrediction(pr)
 
+    # Response Payload
     payload = {
         'output': overall_prediction[0],
         'positive': pos,
@@ -47,35 +61,42 @@ def home():
         'negative': neg
     }
 
+    # Actual Response
     response = jsonify(payload)
 
-    payload['tweet'] = tweet
-    collection.insert_one(payload)
+    # Database Addition
+    database.handleDocumentCreation(payload, tweet)
+
     return response
+
+# Stats Route
 
 
 @app.route("/api/v1/stats", methods=['POST'])
 def stats():
 
+    # Handling Request Body Requirements
     if request.json['tweet'] == '':
         return jsonify({
             'error': 'Empty String'
         })
 
-    collection = dataBase['other-algorithms']
-
+    # Getting Body
     tweet = request.json['tweet']
     model_name = request.json['model_name']
 
+    # Object Init
     pre = PreProcessor(tweet)
     pr = pre.preProcess()
     algo = MLAlgorithms(model_name=model_name)
 
+    # Applying Algorithms
     accuracy_score = algo.accuracy_score
     confusion_matrix_path = algo.confusion_matrix[1:]
     overall_prediction = algo.getOverallPrediction(pr)
     pos, neu, neg = algo.getPercentagePrediction(pr)
 
+    # Response Payload
     payload = {
         'output': overall_prediction[0],
         'positive': pos,
@@ -86,14 +107,15 @@ def stats():
         'model_name': model_name
     }
 
+    # Actual Response
     response = jsonify(payload)
 
-    payload['tweet'] = tweet
-    collection.insert_one(payload)
+    # Database Addition
+    database.handleDocumentCreation(payload, tweet, isStats=True)
 
     return response
 
 
 if __name__ == '__main__':
-    # serve(app, host='0.0.0.0', port=7000) # For Production
-    app.run(debug=True, port=7000)  # For Development
+    # serve(app, host='0.0.0.0', port=PORT)  # For Production
+    app.run(debug=True, port=PORT)  # For Development
