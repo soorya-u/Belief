8dff3fefacc577a90155b7a9af2e9398c4c994bf
Soorya U <sooryau7@gmail.com>
2024-02-19 22:07:31 +0530

    feat: Connected New Endpoint
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 import About from "./components/About";
 import Home from "./components/Home";
@@ -7,6 +9,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
+  
+  useEffect(() => {
+    async function wakeServer() {
+      await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/v1/wake`);
+    }
+    wakeServer();
+  });
+
   return (
     <>
       <Header />
