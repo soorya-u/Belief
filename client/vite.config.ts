a76a279675b1b1c5938b0089166bfa343fca5cca
Soorya U <sooryau7@gmail.com>
2024-02-21 21:06:42 +0530

    feat: Added Path Alias-2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+
+const root = resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(root),
+    },
+  },
+});
