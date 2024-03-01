87366e280da15f129a2d683896d0046abd1f0895
Soorya U <sooryau7@gmail.com>
2024-03-01 20:16:37 +0530

    chore: TanStack Query for WakeUp Route Handler
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
-import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+
+import { AxiosService } from "./libs/axios";
+import { useQuery } from "@tanstack/react-query";
 
 import NavBar from "@/components/custom/NavBar";
 import Home from "@/pages/Home";
@@ -7,15 +9,15 @@ import About from "@/pages/About";
 import Stats from "@/pages/Stats";
 import Footer from "@/components/custom/Footer";
 import Wrapper from "@/components/custom/Wrapper";
-import { AxiosService } from "./libs/axios";
 
 function App() {
-  useEffect(() => {
-    async function wakeServer() {
-      await AxiosService.wakeServer();
-    }
-    wakeServer();
+  const { error } = useQuery({
+    queryKey: ["wake"],
+    queryFn: async () => await AxiosService.wakeServer(),
+    refetchInterval: 1000 * 60 * 10, // 10 Minutes
   });
+
+  if (error) return;
 
   return (
     <>
