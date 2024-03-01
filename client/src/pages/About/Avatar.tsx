c8ddfed72f95064deebbb6e7de1f969baa9b5bd4
Soorya U <sooryau7@gmail.com>
2024-03-01 20:55:38 +0530

    fix: Minor Style Fix
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/pages/About/Avatar.tsx
+++ client/src/pages/About/Avatar.tsx
@@ -10,7 +10,7 @@ function Avatar({
   return (
     <>
       <div className="h-[300px] group">
-        <div className="w-[12rem] h-[12rem] overflow-hidden relative flex justify-center items-center z-[1] duration-[0.7s] translate-y-[35px] group-hover:translate-y-0">
+        <div className="w-[12rem] h-[12rem] overflow-hidden relative flex justify-center items-center z-[1] duration-700 translate-y-[35px] group-hover:translate-y-0">
           <div className="flex justify-center items-center flex-col gap-1">
             <div className="absolute top-0 left-0 w-1/2 h-1/2">
               <img
@@ -22,7 +22,7 @@ function Avatar({
           </div>
         </div>
 
-        <div className="w-[12rem] h-[12rem] overflow-hidden relative flex justify-center items-center box-border duration-[0.8s] -translate-y-[8.5rem] shadow-['0_20px_40px_rgba(0,0,0,0.4)'] group-hover:-translate-y-4">
+        <div className="w-[12rem] h-[12rem] overflow-hidden relative flex justify-center items-center box-border duration-700 -translate-y-[8.5rem] shadow-['0_20px_40px_rgba(0,0,0,0.4)'] group-hover:-translate-y-4">
           <div className="flex justify-center items-center flex-col gap-1">
             <h3 className="mb-[10px] p-0 text-[24px] text-center text-white">
               {name}
