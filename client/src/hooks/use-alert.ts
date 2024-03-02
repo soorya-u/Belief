21ee24ac26e7cf7fba24f7355522cc36fc57711f
Soorya U <sooryau7@gmail.com>
2024-03-02 09:55:49 +0530

    feat: Created useAlert Hook
    Signed-off-by: Soorya U <sooryau7@gmail.com>

---
+++ client/src/hooks/use-alert.ts
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useToast } from "@/components/shadcn/use-toast";
+
+export function useNetworkAlert(error: boolean) {
+  const { toast } = useToast();
+  useEffect(() => {
+    error &&
+      toast({
+        title: "Network Error",
+        description: "There might be a problem with your Internet",
+      });
+  }, [error, toast]);
+}
