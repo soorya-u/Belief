c32e250c8f96beaa4d466bd0a493f2c42ca4cfe1
Soorya U <sooryau7@gmail.com>
2024-03-10 12:50:54 +0530

    chore: Segregated Devs from Component
    Signed-off-by: Soorya U <sooryau7@gmail.com>

---
+++ client/src/utils/devs.ts
@@ -0,0 +1,33 @@
+import dev_soo from "@/assets/img/devs/soorya.jpg";
+import dev_saa from "@/assets/img/devs/saanvi.jpg";
+import dev_ai from "@/assets/img/devs/aishwarya.jpg";
+import dev_de from "@/assets/img/devs/devi.jpg";
+import dev_pr from "@/assets/img/devs/pratham.jpg";
+
+export const devs = [
+  {
+    name: "Soorya U",
+    usn: "4AI21AI052",
+    image: dev_soo,
+  },
+  {
+    name: "Saanvi MJ",
+    usn: "4AI21AI044",
+    image: dev_saa,
+  },
+  {
+    name: "Aishwarya HA",
+    usn: "4AI21AI003",
+    image: dev_ai,
+  },
+  {
+    name: "Devika M",
+    usn: "4AI21AI015",
+    image: dev_de,
+  },
+  {
+    name: "Pratham KR",
+    usn: "4AI21AI039",
+    image: dev_pr,
+  },
+];
