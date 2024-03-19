1b65a6b6f5f873f5b6ffafb562a029aa854dd948
Soorya U <sooryau7@gmail.com>
2024-03-19 21:43:07 +0530

    fix: Fixed PowerShell Error
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- scripts/start-dev.ps1
+++ scripts/start-dev.ps1
@@ -1,6 +1,8 @@
 function main {
   $env = Read-Host "What Environment do you want the app to run on? Docker(d)/Local(l)"
 
+  Write-Host
+
   switch ($env) {
     "d" {
       dockerSetup
@@ -19,7 +21,7 @@ function main {
 
 function dockerSetup {
   docker-compose -f .\scripts\docker-compose.yml -p belief up -d > $null 2>&1
-  Write-Host "`n Container has been Successfully Spun Up `n"
+  Write-Host "Container has been Successfully Spun Up"
   exit 0
 }
 
@@ -27,39 +29,53 @@ function localSetup {
   $client_port = 5000
   $server_port = 9000
 
-  $node_package_manager = "npm", "yarn", "pnpm" | Where-Object { Test-Path (Get-Command $_).Path }
+  $package_managers = @("pnpm", "yarn", "npm")
+  $node_package_manager = $null
+  
+  foreach ($manager in $package_managers) {
+    $executable = Join-Path $env:APPDATA "npm\$manager.cmd"
+    if (Test-Path $executable) {
+      $node_package_manager = $executable
+      break
+    }
+  }
 
   if (-not (Test-Path .\client\node_modules)) {
-    Write-Host "Initalizing node_modules `n"
+    Write-Host "Initializing node_modules"
     Set-Location .\client
-    &$node_package_manager i > $null
+    Invoke-Expression "$node_package_manager i" > $null
     Set-Location ..
-    Write-Host "node_modules Initialized `n`n"
+    Write-Host "node_modules Initialized `n"
   }
 
   if (-not (Test-Path .\server\env)) {
-    Write-Host "Creating Python Virtual Environment `n"
+    Write-Host "Creating Python Virtual Environment"
     Set-Location .\server
-    python3 -m venv env
-    .\env\Scripts\Activate
-    pip3 install -r requirements.txt > $null
+    python -m venv env
+    .\env\Scripts\Activate.ps1
+    pip install -r requirements.txt > $null 2>&1
     Set-Location ..
-    Write-Host "Created Python Virtual Environment `n`n"
+    Write-Host "Created Python Virtual Environment `n"
   }
 
+  
   Set-Location .\client
-  Start-Process -FilePath "$node_package_manager.cmd" -ArgumentList "dev", "--port", $client_port -NoNewWindow
-  $client_process = Get-Process -Name "node"
-
+  $client_process = Start-Process -FilePath "$node_package_manager" -ArgumentList "dev", "--port", $client_port, ">", "`$null", "2>&1" -NoNewWindow -PassThru
+  
+  Set-Location ..
+  .\server\env\Scripts\Activate.ps1
   Set-Location .\server
-  Start-Process -FilePath "python3" -ArgumentList "-m", "flask", "run", "--debug", "--port=$server_port" -NoNewWindow
-  $server_process = Get-Process -Name "python"
+  $server_process = Start-Process -FilePath "python" -ArgumentList "-m", "flask", "run", "--debug", "--port=$server_port",  -NoNewWindow -PassThru -WindowStyle Hidden
+    
+  Set-Location ..
 
-  Write-Host "Development Server started! `n`n"
-  Write-Host "Client is running at http://localhost:$client_port `n"
-  Write-Host "Server is running at http://loaclhost:$server_port `n"
+  Write-Host "Development Server started!`n"
+  Write-Host "Client is running at http://localhost:$client_port"
+  Write-Host "Server is running at http://localhost:$server_port`n"
 
-  Wait-Process $client_process, $server_process
+  $client_process.waitForExit()
+  $server_process.waitForExit()
+
 }
 
 main
--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -1,80 +1,83 @@
 #!/bin/bash
 
 main() {
-
-printf "What Environment do you want the app to run on? Docker(d)/Local(l): "
-read env
-
-case $env in
-
-  "d")
-    dockerSetup
-    ;;
-
-  "l")
-    localSetup
-    ;;
   
-  *) 
-    exit 1
+  printf "What Environment do you want the app to run on? Docker(d)/Local(l): "
+  read env
+  
+  echo
+  
+  case $env in
+    
+    "d")
+      dockerSetup
     ;;
-
-esac
-
+    
+    "l")
+      localSetup
+    ;;
+    
+    *)
+      exit 1
+    ;;
+    
+  esac
+  
 }
 
 
 dockerSetup() {
   docker-compose -f ./scripts/docker-compose.yml -p belief up -d &> /dev/null
-  printf "\n Container has been Successfully Spun Up \n"
+  echo "Container has been Successfully Spun Up"
   exit 0
 }
 
 localSetup() {
-scripting_shell="$(readlink /proc/$$/exe | sed "s/.*\///")"
-client_port=5000
-server_port=9000
-
-for cmd in pnpm yarn npm; do
+  scripting_shell="$(readlink /proc/$$/exe | sed "s/.*\///")"
+  client_port=5000
+  server_port=9000
+  
+  for cmd in pnpm yarn npm; do
     if command -v $cmd > /dev/null; then
-        node_package_manager=$cmd 
-        break
+      node_package_manager=$cmd
+      break
     fi
-done
-
-if ! test -d ./client/node_modules; then
-  printf "Initalizing node_modules \n"
-  cd client
-  command $node_package_manager i > /dev/null
-  cd ..
-  printf "node_modules Initialized \n\n"
-fi
-
-if ! test -d ./server/env; then
-  printf "Creating Python Virtual Environment \n"
-  cd server
-  python3 -m venv env
-  source env/bin/activate
-  pip3 install -r requirements.txt &> /dev/null
-  cd ..
-  printf "Created Python Virtual Environment \n\n"
-fi
-
-cd client && command $node_package_manager dev --port $client_port > /dev/null  &
-client_process=$!
-
-if [ "$scripting_shell" = "zsh" ]; then
-  cd ..
-fi
-
-cd server && python3 -m flask run --debug --port=$server_port &> /dev/null &
-server_process=$!
-
-printf "Development Server started! \n\n"
-printf "Client is running at http://localhost:$client_port \n"
-printf "Server is running at http://loaclhost:$server_port \n"
-
-wait $client_process $server_process
+  done
+  
+  if ! test -d ./client/node_modules; then
+    printf "Initalizing node_modules"
+    cd client
+    command $node_package_manager i > /dev/null
+    cd ..
+    printf "node_modules Initialized \n\n"
+  fi
+  
+  if ! test -d ./server/env; then
+    printf "Creating Python Virtual Environment"
+    cd server
+    python3 -m venv env
+    source env/bin/activate
+    pip3 install -r requirements.txt &> /dev/null
+    cd ..
+    printf "Created Python Virtual Environment \n\n"
+  fi
+  
+  cd client && command $node_package_manager dev --port $client_port > /dev/null  &
+  client_process=$!
+  
+  if [ "$scripting_shell" = "zsh" ]; then
+    cd ..
+  fi
+  
+  source ./server/env/bin/activate
+  cd server && python3 -m flask run --debug --port=$server_port &> /dev/null &
+  server_process=$!
+  
+  printf "Development Server started! \n\n"
+  printf "Client is running at http://localhost:$client_port \n"
+  printf "Server is running at http://loaclhost:$server_port \n"
+  
+  wait $client_process $server_process
 }
 
 main
