7f125d21b13717f31ea97603fa2702fe14d3a632
Soorya U <sooryau7@gmail.com>
2024-04-16 17:56:37 +0530

    feat: New Footer
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/components/custom/Footer/index.tsx
+++ client/src/components/custom/Footer/index.tsx
@@ -1,48 +1,41 @@
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-const Footer = () => {
+export default function Footer() {
   return (
-    <>
-      <footer className="flex justify-center items-center flex-col gap-4  mt-12 mb-6">
-        <section className="sticky flex flex-col justify-center w-[90vw] items-center border-t-[3px] border-t-[#202630] gap-4 2xs:flex-row 2xs:justify-between 2xs:items-start">
-          <div className="flex justify-center items-center flex-col gap-0 2xs:gap-4">
-            <h2 className="text-[1.5rem] font-['Chakra_Petch'] text-[#1da1f2] font-extrabold">
-              Our Projects
-            </h2>
-            <h4 className="self-center 2xs:self-start">
-              <a
-                className="text-[1.2rem] no-underline font-['Audiowide']"
-                href="https://grade-grove.soorya-u.dev/"
-              >
-                Grade Grove
-              </a>
-            </h4>
-          </div>
-
-          <div className="flex justify-center items-center flex-col gap-2 2xs:gap-4">
-            <h2 className="text-[1.5rem] font-['Chakra_Petch'] text-[#1da1f2] font-extrabold">
-              Get in Touch
-            </h2>
-            <div className="flex justify-center items-center gap-6">
-              <a href="https://github.com/soorya-u/Belief" target="_blank">
-                <FontAwesomeIcon icon={faGithub} className="text-[2rem]" />
-              </a>
-
-              <a href="mailto:sooryau7@gmail.com" target="_blank">
-                <FontAwesomeIcon icon={faEnvelope} className="text-[2rem]" />
-              </a>
-
-              <a href="https://www.linkedin.com/in/soorya-u/" target="_blank">
-                <FontAwesomeIcon icon={faLinkedin} className="text-[2rem]" />
-              </a>
-            </div>
-          </div>
-        </section>
-      </footer>
-    </>
+    <footer className="flex flex-col justify-between items-center min-h-16 w-[85vw] m-auto gap-y-4 border-t-[2px] border-t-[#ffffff49] md-lg:flex-row py-3">
+      <div className="flex justify-center gap-x-2 items-center gap-y-1 flex-wrap">
+        <img src="/logo.png" width={28} height={28} alt="Elite-AIML-Logo" />
+        <p className={"text-gray-200 text-center text-['Prompt']"}>
+          Built by{" "}
+          <a
+            href="https://twitter.com/sooryaa_u"
+            className="underline underline-offset-2"
+            target="_blank"
+          >
+            soorya-u
+          </a>
+          . The source code is available on{" "}
+          <a
+            href="https://github.com/soorya-u/Belief"
+            className="underline underline-offset-2"
+            target="_blank"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+      </div>
+      <div className="flex justify-center items-center gap-x-2 gap-y-1 flex-wrap">
+        <p className={"text-gray-200 text-center text-['Prompt']"}>
+          Check these Projects Out:
+        </p>
+        <a href="https://grade-grove.soorya-u.dev" target="_blank">
+          <img
+            src="https://grade-grove.soorya-u.dev/logo.png"
+            width={28}
+            height={28}
+            alt="Grade-Grove-Logo"
+          ></img>
+        </a>
+      </div>
+    </footer>
   );
-};
-
-export default Footer;
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,6 +16,7 @@ export default {
       xs: "450px",
       sm: "640px",
       md: "768px",
+      "md-lg": "875px",
       lg: "1024px",
       xl: "1280px",
       "2xl": "1400px",
